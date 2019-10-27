a581af611a29d21dbce6b7c4ee3053ac4dbd3e29
Matthew Malone <m.malone@me.com>
2019-10-26 19:05:13 -0600

    Show error message if wrong email and/or password are provided

--- src/Components/LoginForm/LoginForm.js
+++ src/Components/LoginForm/LoginForm.js
@@ -36,51 +36,58 @@ export class LoginForm extends React.Component {
 
   render() {
     const isEnabled = this.canBeSubmitted();
-    return(
-      <form className={this.props.currentUser.id ? 'header--login__hidden' : 'header--login'}>
-        <div className="header--userinput__div">
-          <label
-            className="login__label"
-            htmlFor="email">Email</label>
-          <input
-            className="header--userinput__input"
-            id="email"
-            type="text"
-            name="email"
-            placeholder="Email"
-            onChange={this.handleChnage}
-            value={this.state.email} />
-          <p className="login--bottomtext new--account__btn"
-            onClick={ this.props.toggleModal }
-          >Create new account</p>
+    return (
+      <div className="errorHolder__div">
+        <form className={this.props.currentUser.id ? 'header--login__hidden' : 'header--login'}>
+          <div className="header--userinput__div">
+            <label
+              className="login__label"
+              htmlFor="email">Email</label>
+            <input
+              className={this.props.error ? "header--userinput__input error__input" : "header--userinput__input"}
+              id="email"
+              type="text"
+              name="email"
+              placeholder="Email"
+              onChange={this.handleChnage}
+              value={this.state.email} />
+            <p className="login--bottomtext new--account__btn"
+              onClick={ this.props.toggleModal }
+            >Create new account</p>
+          </div>
+          <div className="header--userinput__div">
+            <label
+              className="login__label"
+              htmlFor="password">Password</label>
+            <input
+              className={this.props.error ? "header--userinput__input error__input" : "header--userinput__input"}
+              id="password"
+              type="password"
+              name="password"
+              placeholder="Password"
+              onChange={this.handleChnage}
+              value={this.state.password} />
+            <p className="login--bottomtext" >Forgot account?</p>
+          </div>
+          <button
+            type="button"
+            className="login__button"
+            onClick={(event) => this.handleSubmit(event)} 
+            disabled={!isEnabled}>Login</button>
+        </form>
+        <div className="error--text__div">
+          <p className={this.props.error ? "error--text" : "error--text__div__hidden" }
+          >The email and/or passowrd you've entered is incorrect.</p>
         </div>
-        <div className="header--userinput__div">
-          <label
-            className="login__label"
-            htmlFor="password">Password</label>
-          <input
-            className="header--userinput__input"
-            id="password"
-            type="password"
-            name="password"
-            placeholder="Password"
-            onChange={this.handleChnage}
-            value={this.state.password} />
-          <p className="login--bottomtext" >Forgot account?</p>
-        </div>
-        <button
-          type="button"
-          className="login__button"
-          onClick={(event) => this.handleSubmit(event)} 
-          disabled={!isEnabled}>Login</button>
-      </form>
+      </div>
     )
   }
 }
 
-const mapStateToProps = ({ toggleModal, currentUser }) => ({
+const mapStateToProps = ({ toggleModal, currentUser,error }) => ({
   toggleModal,
-  currentUser
+  currentUser,
+  error
 })
 
 const mapDispatchToProps = (dispatch) => (
