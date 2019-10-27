0b33aada16327d3999010c6752da5efc6a42b2d3
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-27 13:32:14 -0600

    Add think for adding a favorite movie

---
+++ src/Thunks/addFavorite.js
@@ -0,0 +1,34 @@
+import { isLoading, hasErrored, setFavorites } from '../Actions'
+
+export const addFavorite = (movieInfo) => {
+  const { id, title, poster_path, release_date, vote_average, overview } = movieInfo;
+  const cleanedInfo = {
+    movie_id: id,
+    title,
+    poster_path,
+    release_date,
+    vote_average,
+    overview
+  }
+  const options = {
+    method: 'POST',
+    body: JSON.stringify(cleanedInfo),
+    headers: {
+      'Content-Type': 'application/json'
+    }
+  };
+  return async (dispatch) => {
+    try {
+      dispatch(isLoading(true))
+      const response = await fetch(`http://localhost:3001/api/v1/users/${id}/moviefavorites`, options)
+      if (!response.ok) {
+        throw Error(response.statusText)
+      }
+      const favoritesResponse = await response.json()
+      dispatch(isLoading(false));
+      dispatch(setFavorites(favoritesResponse));
+    } catch (error) {
+      dispatch(hasErrored(error.message))
+    }
+  }
+}
