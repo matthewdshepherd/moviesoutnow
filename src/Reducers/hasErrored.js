39110d3836d7ad51f4b51ea40a66687f69a0e995
Matthew Malone <m.malone@me.com>
2019-10-26 19:08:17 -0600

    Update hasErrored reducer to work with clear error action

--- src/Reducers/hasErrored.js
+++ src/Reducers/hasErrored.js
@@ -2,6 +2,8 @@ export const hasErrored = (state = '', action) => {
   switch (action.type) {
     case 'HAS_ERRORED':
       return action.msg
+    case 'CLEAR_ERROR':
+    return ""
     default:
       return state
   }
