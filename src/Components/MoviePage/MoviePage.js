4c183672256c6e228d462a3cdcd9fd9411f49b79
Matthew Malone <m.malone@me.com>
2019-10-28 17:28:35 -0600

    Import movie details

--- src/Components/MoviePage/MoviePage.js
+++ src/Components/MoviePage/MoviePage.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import './MoviePage.css'
 
-const MoviePage = ({ title, poster_path, backdrop_path, release_date, vote_average, vote_count, overview, genre, isFavorite, id }) => {
-
+const MoviePage = ({ title, poster_path, backdrop_path, release_date, vote_average, vote_count, overview, genre_ids, isFavorite, id }) => {
+    getGenres = () => {
+        reutrn "genre"
+    }
 
     return (
         <div className="moviePage__div" key={id}>
@@ -21,12 +23,12 @@ const MoviePage = ({ title, poster_path, backdrop_path, release_date, vote_avera
                         <p className="movie--year">{release_date.split('-')[0]}</p>
                     </div>
                     <div className="movie--user--score--fav--votes">
-                        <p className="movie--score">86</p>
+                        <p className="movie--score">User Score: {vote_average.split('.').join('')}</p>
                         <p className="movie--favorited">Fav</p>
-                        <p className="movie--vote">Vote</p>
+                        <p className="movie--vote">Votes: {vote_count}</p>
                     </div>
-                    <p className="movie--overview"></p>
-                    <div className="movie--genres"></div>
+                    <p className="movie--overview">{overview}</p>
+                    <div className="movie--genres">{getGenres}</div>
                 </div>
             </div>
         </div>
