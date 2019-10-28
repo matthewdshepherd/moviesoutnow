bf29fae1ad4a204d15225407c09bb4fb5bb115dc
Matthew Malone <m.malone@me.com>
2019-10-28 16:37:33 -0600

    Dynamically add movie details to MoviePage Componemt

--- src/Components/MoviePage/MoviePage.js
+++ src/Components/MoviePage/MoviePage.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import './MoviePage.css'
 
-const MoviePage = ({}) => {
+const MoviePage = ({ title, poster_path, backdrop_path, release_date, vote_average, vote_count, overview, genre, isFavorite, id }) => {
 
 
     return (
-        <div className="moviePage__div">
-            {/* image - movie background */}
+        <div className="moviePage__div" key={id}>
+            <img 
+                src={`https://image.tmdb.org/t/p/w500${backdrop_path}`} 
+                alt={`${title} Back Drop`}
+                className="moviePage--backdrop" />
             <div className="movie--poster--details__div">
-                {/* image - movie pster */}
+                <img
+                src={`https://image.tmdb.org/t/p/w500${poster_path}`} 
+                alt={`${title} Poster`}
+                className="moviePage--poster" />
                 <div className="movie__details">
                     <div className="movie--title--year">
-                        <h3 className="movie--title">Movie Title</h3>
-                        <p className="movie--year">Movie Year</p>
+                        <h3 className="movie--title">{title}</h3>
+                        <p className="movie--year">{release_date.split('-')[0]}</p>
                     </div>
                     <div className="movie--user--score--fav--votes">
                         <p className="movie--score">86</p>
@@ -26,3 +32,5 @@ const MoviePage = ({}) => {
         </div>
     ) 
 }
+
+export default MoviePage
