1c8f2923ee9952a9822708816a783e20ef73355c
Matthew Malone <m.malone@me.com>
2019-10-28 16:19:12 -0600

    Finish HTML structure for MoviePage component and start css

--- src/Components/MoviePage/MoviePage.js
+++ src/Components/MoviePage/MoviePage.js
@@ -14,7 +14,13 @@ const MoviePage = ({}) => {
                         <h3 className="movie--title">Movie Title</h3>
                         <p className="movie--year">Movie Year</p>
                     </div>
-                    
+                    <div className="movie--user--score--fav--votes">
+                        <p className="movie--score">86</p>
+                        <p className="movie--favorited">Fav</p>
+                        <p className="movie--vote">Vote</p>
+                    </div>
+                    <p className="movie--overview"></p>
+                    <div className="movie--genres"></div>
                 </div>
             </div>
         </div>
