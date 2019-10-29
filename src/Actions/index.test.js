c24411b5958cbc7dabc4bebf795e943111414139
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-29 11:52:42 -0600

    Add test for isLoading action

--- src/Actions/index.test.js
+++ src/Actions/index.test.js
@@ -1,7 +1,13 @@
 import * as actions from '../Actions';
 
 describe('actions', () => {
-  it('should have a type of ADD_TODO', () => {
-    expect(true).toEqual(true);
+  it('should have a type of IS_LOADING', () => {
+    const expectedAction = {
+      type: 'IS_LOADING',
+      isLoading: true
+    };
+    const result = actions.isLoading(true);
+
+    expect(result).toEqual(expectedAction);
   });
 });
