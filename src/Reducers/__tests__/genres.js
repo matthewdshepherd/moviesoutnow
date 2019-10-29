1a038fc5e55f7483074686475f48a12e6e21eebd
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-29 13:21:40 -0600

    Add test for first case in genres reducer

--- src/Reducers/__tests__/genres.js
+++ src/Reducers/__tests__/genres.js
@@ -7,4 +7,16 @@ describe('genres reducer', () => {
 
     expect(result).toEqual(expected);
   });
-})
+
+  it('should return an array of genres', () => {
+    const mockGenres = [{ name: 'Action' }, { name: 'Horror' }]
+    const mockAction = {
+      type: 'SET_GENRES',
+      genres: mockGenres
+    };
+    const expected = mockGenres;
+    const result = genres([], mockAction);
+
+    expect(result).toEqual(expected);
+  });
+});
