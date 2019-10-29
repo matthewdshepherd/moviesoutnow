bd669cc02877106f8ecc675f7a85dee2cce690db
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-29 14:55:36 -0600

    Add test for mapStateToProps in currentUser component

--- src/Components/CurrentUser/CurrentUser.test.js
+++ src/Components/CurrentUser/CurrentUser.test.js
@@ -5,16 +5,28 @@ import { signOut } from '../../Actions';
 
 
 describe('CurrentUser', () => {
-  let wrapper
+    let wrapper
 
-  beforeEach(() => {
-      wrapper = shallow(
-          <CurrentUser
-            currentUser = {{id: 3, name: 'Chris'}}
-          />)
-  })
+    beforeEach(() => {
+        wrapper = shallow(
+            <CurrentUser
+                currentUser={{ id: 3, name: 'Chris' }}
+            />)
+    })
 
     it('should match the snapshot with all data passed in correctly', () => {
         expect(wrapper).toMatchSnapshot();
     });
+});
+
+describe('mapStateToProps', () => {
+    it('should return an object with the current user', () => {
+        const mockState = {
+            currentUser: { id: 3, name: 'Chris' }
+        };
+        const expected = { currentUser: { id: 3, name: 'Chris' } }
+        
+        const mappedProps = mapStateToProps(mockState);
+        expect(mappedProps).toEqual(expected);
+    })
 })
