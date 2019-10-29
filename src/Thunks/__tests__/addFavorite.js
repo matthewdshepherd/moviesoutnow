d365dd8e419b4c2d6cdee22733f0fcc51d631456
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-28 23:23:42 -0600

    Add setup for addFavorite thunk testing

---
+++ src/Thunks/__tests__/addFavorite.js
@@ -0,0 +1,29 @@
+import { addFavorite } from '../addFavorite';
+import { fetchFavorites } from '../fetchFavorites';
+import { isLoading, hasErrored, setFavorites } from '../../Actions';
+
+describe('addFavorite', () => {
+  let mockDispatch
+  let mockEvent
+  let mockMovie
+  let mockUserId
+
+  beforeEach(() => {
+    mockEvent = {
+      preventDefault: jest.fn()
+    };
+    mockMovie = {
+      id: 2,
+      title: 'Good Movie',
+      poster_path: '/someimage',
+      release_date: 'yesterday',
+      vote_average: 33,
+      overview: 'Here is a synopsis'
+    };
+    mockUserId = 3;
+    mockDispatch = jest.fn();
+    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+      ok: true,
+    }))
+  })
+})
