9aaf77149e5275e279870adf602712b9e859f083
Chris Basham <chrisdbasham0317@gmail.com>
2019-10-29 00:17:40 -0600

    Add test for fetch call in postNewUser thunk

--- src/Thunks/__tests__/postNewUser.js
+++ src/Thunks/__tests__/postNewUser.js
@@ -30,5 +30,25 @@ describe('postNewUser', () => {
     thunk(mockDispatch);
 
     expect(mockDispatch).toHaveBeenCalledWith(isLoading(true));
-  });  
+  });
+  
+  it('should call fetch with correct url and options', () => {
+    const mockUrl = 'http://localhost:3001/api/v1/users/'
+    const mockOptions = {
+      method: 'POST',
+      body: JSON.stringify({
+        name: 'Chris Chris',
+        email: 'chris@me.com',
+        password: 'password'
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    };
+    const thunk = postNewUser(mockLoginCreds);
+    thunk(mockDispatch);
+
+    expect(window.fetch).toHaveBeenCalledWith(mockUrl, mockOptions);
+  });
+  
 })
